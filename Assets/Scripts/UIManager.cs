f30d91d4a09fb83ebd74ab5ea1e79462b8397003
iMixBlue <imixgold@gmail.com>
2023-11-26 03:37:06 +0000

    add function-A

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject scoreText;
+    public float totalScore = 0f;
+    private Color defaultColor = Color.black;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void addScore(float score, Color? color = null){
+        totalScore += score;
+        this.scoreText.GetComponent<TMP_Text>().text = "Score : " + totalScore;
+        if(color == null){
+            color = Color.black;
+            this.scoreText.GetComponent<TMP_Text>().color = (Color)color;
+        }
+        else if(color != null){
+            this.scoreText.GetComponent<TMP_Text>().color = (Color)color;
+        }
+    }
+}
